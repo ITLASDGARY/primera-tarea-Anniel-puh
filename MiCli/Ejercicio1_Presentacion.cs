f930ca72ec3b0abe095215e32e68b17002416d72
Anniel-puh <anniel1815@outlook.com>
2026-01-30 01:11:14 +0000

    Completado

--- MiCli/Ejercicio1_Presentacion.cs
+++ MiCli/Ejercicio1_Presentacion.cs
@@ -1,18 +1,11 @@
-namespace MiCli;
+using System;
 
-// TODO: Crea un programa que imprima tu Nombre, Carrera y Hobby favorito en líneas separadas
 public class Ejercicio1_Presentacion
 {
-    public static string ObtenerPresentacion()
-    {
-        // TODO: Declara tres variables string: nombre, carrera y hobby
-        // TODO: Retorna un string con las tres variables en líneas separadas
-        return "reemplaza este texto con tu presentación"; //OJO: reemplaza este texto
-        throw new NotImplementedException();
-    }
-    
     public static void Ejecutar()
     {
-        Console.WriteLine(ObtenerPresentacion());
+        Console.WriteLine("Anniel Rivera");
+        Console.WriteLine("Ingeniería en Sistemas");
+        Console.WriteLine("Ver Novelas");
     }
 }
--- MiCli/Ejercicio2_Sumadora.cs
+++ MiCli/Ejercicio2_Sumadora.cs
@@ -1,26 +1,13 @@
-namespace MiCli;
+using System;
 
-// TODO: Declara dos variables int con valores fijos, súmalas y muestra el resultado
 public class Ejercicio2_Sumadora
 {
-    public static int Sumar(int a, int b)
-    {
-        // TODO: Implementa la suma de a y b
-        return 0; //OJO: reemplaza este texto con el resultado de la suma
-        throw new NotImplementedException();
-    }
-    
-    public static string ObtenerResultado()
-    {
-        // TODO: Declara dos variables int con valores fijos
-        // TODO: Implementa la suma de a y b, y guarda el resultado
-        // TODO: Retorna el texto "La suma es: X" donde X es el resultado
-        return "reemplaza este texto con el resultado de la suma"; //OJO: reemplaza este texto
-        throw new NotImplementedException();
-    }
-    
     public static void Ejecutar()
     {
-        Console.WriteLine(ObtenerResultado());
+        int a = 5;
+        int b = 3;
+        int suma = a + b;
+
+        Console.WriteLine("La suma es: " + suma);
     }
 }
